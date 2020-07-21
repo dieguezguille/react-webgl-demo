2eda1021663d969a6dd6dd17d01100b5f14579b7
dieguezguille <guillermodieguez@outlook.com>
2020-07-21 09:20:14 -0300

    Updates

---
+++ threeact-test/src/components/Box/index.tsx
@@ -0,0 +1,32 @@
+import React, { useState, useRef } from 'react';
+import { useFrame } from 'react-three-fiber';
+
+function Box(props: any) {
+  // This reference will give us direct access to the mesh
+  const mesh = useRef<THREE.Mesh>()
+  // Set up state for the hovered and active state
+  const [hovered, setHover] = useState(false)
+  const [active, setActive] = useState(false)
+
+  // Rotate mesh every frame, this is outside of React without overhead
+    useFrame(() => {
+        if (mesh.current !== undefined) {
+            mesh.current.rotation.x = mesh.current.rotation.y += 0.01
+        }
+    })
+
+  return (
+    <mesh
+      {...props}
+      ref={mesh}
+      scale={active ? [1.5, 1.5, 1.5] : [1, 1, 1]}
+      onClick={(e) => setActive(!active)}
+      onPointerOver={(e) => setHover(true)}
+      onPointerOut={(e) => setHover(false)}>
+      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
+      <meshStandardMaterial attach="material" color={hovered ? 'hotpink' : 'orange'} />
+    </mesh>
+  )
+}
+
+export default Box;
