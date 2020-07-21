34eb5ab57997251934609c80f33042ae9fa58aa7
Guillermo Diéguez <gdieguez@silentiumapps.com>
2020-07-21 11:31:57 -0300

    Added styles

--- threeact-test/src/index.tsx
+++ threeact-test/src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
 import * as serviceWorker from './serviceWorker';
-import './styles.css';
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
