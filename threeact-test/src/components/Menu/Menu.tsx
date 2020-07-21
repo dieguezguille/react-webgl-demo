01e0181684365d6bdc5aadd417cd985764637bc1
Guillermo Diéguez <gdieguez@silentiumapps.com>
2020-07-21 18:04:25 -0300

    Added some scss

--- threeact-test/src/components/Menu/Menu.tsx
+++ threeact-test/src/components/Menu/Menu.tsx
@@ -4,7 +4,7 @@ import Nav from "react-bootstrap/Nav";
 function Menu() {
   return (
     <div className="ui">
-      <h2>Viking Room</h2>
+      <h2 className="title">Viking Room</h2>
       <Nav defaultActiveKey="/home" className="flex-column">
         <Nav.Link>The Tales</Nav.Link>
         <Nav.Link>The Weapons</Nav.Link>
--- threeact-test/src/index.tsx
+++ threeact-test/src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './components/App/App';
 import * as serviceWorker from './serviceWorker';
 import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import './custom.scss';
 
 ReactDOM.render(
   <React.StrictMode>
