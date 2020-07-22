b5a4df71b93037aa4036c93742799a24a18e006b
Guillermo Diéguez <gdieguez@silentiumapps.com>
2020-07-22 09:44:02 -0300

    Added loading

--- threeact-test/src/components/App/App.tsx
+++ threeact-test/src/components/App/App.tsx
@@ -1,8 +1,14 @@
 import React, { Suspense } from "react";
 import { Canvas } from "react-three-fiber";
 import RoomModel from "../../models/viking-room/VikingRoom";
-import { OrbitControls } from "drei";
+import { OrbitControls, HTML } from "drei";
 import Menu from "../Menu/Menu";
+
+const Fallback = () => (
+  <HTML>
+    <h2 className="loading">Loading...</h2>
+  </HTML>
+)
 
 function App() {
   return (
@@ -11,7 +17,7 @@ function App() {
       <Canvas camera={{ position: [25, 18, 15] }}>
         <ambientLight />
         <pointLight position={[10, 10, 10]} />
-        <Suspense fallback={null}>
+        <Suspense fallback={<Fallback></Fallback>}>
           <RoomModel position={[0, 0, 0]} />
         </Suspense>
         <OrbitControls />
